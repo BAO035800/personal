52d13d151db8aa408f16ec6f2f383d9e8e096cc6
BAO035800 <baomoren50@gmail.com>
2024-10-22 20:34:27 +0700

    12345

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,61 @@
 
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
+//slide chuột//
+window.onscroll = function() { 
+  if (window.innerWidth > 740) {
+      scrollFunction(); 
+  }
+};
+
+window.onresize = function() {
+  if (window.innerWidth <= 739) {
+    resetHeaderStyles();
+  }
+}
 
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      document.getElementById("header").style.height = "6rem";
-      document.getElementById("logo-personal").style.marginLeft="30px"
-    } else {
-      document.getElementById("header").style.height = "8rem";
-      document.getElementById("logo-personal").style.marginLeft="initial";
-    }
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      document.getElementById("header").style.height = "5rem";
+      document.getElementById("logo-personal").style.marginLeft = "6vw";
+      document.querySelector(".menu").style.marginRight = "3vw"; 
+      const listItems = document.querySelectorAll("li");
+      listItems.forEach(function(item) {
+          item.style.lineHeight = "5rem";
+      });
+  } else {
+      document.getElementById("header").style.height = "7rem";
+      document.getElementById("logo-personal").style.marginLeft = "initial";
+      document.querySelector(".menu").style.marginRight = "initial"; 
+      const listItems = document.querySelectorAll("li");
+      listItems.forEach(function(item) {
+          item.style.lineHeight = "7rem";
+      });
   }
+}
+
+function resetHeaderStyles() {
+  // Tắt transition tạm thời
+  document.getElementById("header").style.transition = "none";
+  document.getElementById("logo-personal").style.transition = "none";
+  document.querySelector(".menu").style.transition = "none";
+
+  // Khôi phục transition sau khi đặt lại xong
+  setTimeout(() => {
+      document.getElementById("header").style.transition = "";
+      document.getElementById("logo-personal").style.transition = "";
+      document.querySelector(".menu").style.transition = "";
+  }, 0);
+}
+//nav//
+function openMenu() {
+  document.getElementsByClassName('menu')[0].style.right = "0"; // Trượt menu vào
+  document.getElementsByClassName('icon-nav')[0].style.display ="none";
+}
+
+function closeMenu() {
+  document.getElementsByClassName('menu')[0].style.right = "-100%"; // Trượt menu ra ngoài
+  setTimeout(() => {
+    document.getElementsByClassName('icon-nav')[0].style.display = "block"; // Hiển thị lại icon sau khi menu đã đóng hoàn toàn
+}, 300); 
+}
+
   
